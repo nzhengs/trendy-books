fa8be4582f37b837b63478833a4202304c06a081
anjanahengaju <45703218+nzhengs@users.noreply.github.com>
2025-06-01 13:11:45 -0400

    Update the title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,7 @@ export default function Header() {
     <header className="p-2 flex gap-2 bg-white text-black justify-between">
       <nav className="flex flex-row">
         <div className="px-2 font-bold">
-          <Link to="/">Home</Link>
-        </div>
-
-        <div className="px-2 font-bold">
-          <Link to="/demo/tanstack-query">TanStack Query</Link>
+          <Link to="/">Trending Books</Link>
         </div>
       </nav>
     </header>
