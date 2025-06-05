7593bb24ecf6bb3daa1bc0d7161ceb2c3b72b6d0
anjanahengaju <45703218+nzhengs@users.noreply.github.com>
2025-06-05 15:23:26 -0400

    Add missing key

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -5,8 +5,8 @@ export type StarsProps = {
 };
 export const Stars = ({ number }: StarsProps) => (
   <span>
-    {[...Array(Math.round(number))].map(() => (
-      <span className="inline-flex">
+    {[...Array(Math.round(number))].map((_, index) => (
+      <span key={index} className="inline-flex">
         <Star />
       </span>
     ))}
