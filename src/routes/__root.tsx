b26bb27e8051c4f9d321b360da07197009af0d2d
anjanahengaju <45703218+nzhengs@users.noreply.github.com>
2025-06-05 15:28:26 -0400

    Add main tag

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,9 +8,9 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <Header />
-      <div className="mx-auto px-4 lg:px-4 xl:px-0 my-4 max-w-5xl mt-12 overflow-hidden font-serif">
+      <main className="mx-auto px-4 lg:px-4 xl:px-0 my-4 max-w-5xl mt-12 overflow-hidden font-serif">
         <Outlet />
-      </div>
+      </main>
     </>
   ),
 });
