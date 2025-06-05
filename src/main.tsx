29e9881ea6c056cd4d8e4b5827cca28b3a91f14e
anjanahengaju <45703218+nzhengs@users.noreply.github.com>
2025-06-05 15:22:43 -0400

    Clean up code

--- src/main.tsx
+++ src/main.tsx
@@ -2,20 +2,16 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 
-import * as TanStackQueryProvider from './integrations/tanstack-query/root-provider.tsx';
-
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
 
 import './styles.css';
 import reportWebVitals from './reportWebVitals.ts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 // Create a new router instance
 const router = createRouter({
   routeTree,
-  context: {
-    ...TanStackQueryProvider.getContext(),
-  },
   defaultPreload: 'intent',
   scrollRestoration: true,
   defaultStructuralSharing: true,
@@ -35,9 +31,9 @@ if (rootElement && !rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <TanStackQueryProvider.Provider>
+      <QueryClientProvider client={new QueryClient()}>
         <RouterProvider router={router} />
-      </TanStackQueryProvider.Provider>
+      </QueryClientProvider>
     </StrictMode>,
   );
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,22 +1,16 @@
-import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
-import type { QueryClient } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import {
+  Outlet,
+  createRootRoute,
+} from '@tanstack/react-router';
 import { Header } from '@/components/Header';
 
-interface MyRouterContext {
-  queryClient: QueryClient;
-}
-
-export const Route = createRootRouteWithContext<MyRouterContext>()({
+export const Route = createRootRoute({
   component: () => (
     <>
       <Header />
       <div className="mx-auto px-4 lg:px-4 xl:px-0 my-4 max-w-5xl mt-12 overflow-hidden font-serif">
         <Outlet />
       </div>
-      <TanStackRouterDevtools />
-      <ReactQueryDevtools />
     </>
   ),
 });
