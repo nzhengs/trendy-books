c3d94b73403269d3595cf3c74ec91ac4dbca81ca
anjanahengaju <45703218+nzhengs@users.noreply.github.com>
2025-06-05 23:27:59 -0400

    Add header sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import { Link } from '@tanstack/react-router';
 
 export const Header = () => {
   return (
-    <header className=" w-full border-b font-mono font-bold">
+    <header className="sticky z-50 w-full border-b font-mono font-bold top-0 bg-teal-50">
       <nav>
-        <div className="px-4 xl:px-0 flex items-center justify-start h-12 sticky max-w-5xl mx-auto ">
+        <div className="px-4 xl:px-0 flex items-center justify-start h-12 max-w-5xl mx-auto">
           <Link to="/">Trendy Books</Link>
         </div>
       </nav>
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,13 @@
-import {
-  Outlet,
-  createRootRoute,
-} from '@tanstack/react-router';
+import { Outlet, createRootRoute } from '@tanstack/react-router';
 import { Header } from '@/components/Header';
 
 export const Route = createRootRoute({
   component: () => (
-    <>
+    <div className="min-h-screen">
       <Header />
-      <main className="mx-auto px-4 lg:px-4 xl:px-0 my-4 max-w-5xl overflow-hidden font-serif">
+      <main className="mx-auto px-4 lg:px-4 xl:px-0 my-4 max-w-5xl overflow-scroll font-serif">
         <Outlet />
       </main>
-    </>
+    </div>
   ),
 });
