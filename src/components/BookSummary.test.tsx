dc15988666fdcd5bef04aaef505ad2436d88586e
anjanahengaju <45703218+nzhengs@users.noreply.github.com>
2025-06-06 10:06:45 -0400

    Clean up test

--- src/components/BookSummary.test.tsx
+++ src/components/BookSummary.test.tsx
@@ -1,19 +1,17 @@
-import { beforeEach, describe, expect, it } from 'vitest';
+import {  describe, expect, it } from 'vitest';
 import { BookSummary } from './BookSummary';
 import { screen } from '@testing-library/react';
 import { expectTextToBeVisible, renderWithProviders } from '@/utils/TestUtils';
 import { book1 } from '@/apis/mocks';
 
 describe('BookSummary', async () => {
-  beforeEach(
-    async () => await renderWithProviders(<BookSummary book={book1} />),
-  );
-
-  it('should render book title correctly', () => {
+  it('should render book title correctly', async () => {
+    await renderWithProviders(<BookSummary book={book1} />);
     expectTextToBeVisible(book1.title!);
   });
 
-  it('should render link ', () => {
+  it('should render link ', async () => {
+    await renderWithProviders(<BookSummary book={book1} />);
     expect(screen.getByRole('link')).toHaveAttribute(
       'href',
       `/books/${book1.id}/${book1.slug}`,
